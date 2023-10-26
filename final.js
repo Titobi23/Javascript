1cf1b3397430a13ac470eece8d39dd8075170498
Titobiloluwa Olatunji <olatunjititobi@gmail.com>
2023-10-27 01:29:31 +0200

    update

--- final.js
+++ final.js
@@ -1,15 +1,15 @@
+let elements = document.getElementsByName("cssProperty");
+
+let div = document.getElementById('modify');
+
 function set() {
     
-    let elements = document.getElementsByName("cssProperty");
-
     for( let index = 0; index < elements.length; index++){
         let cssProperty = elements[index].getAttribute('id');
 
         let cssValue = elements[ index ].value ;
 
         console.log( cssProperty, cssValue);
-
-        let div = document.getElementById('modify');
 
         div.style[ cssProperty ] = cssValue;
     }
